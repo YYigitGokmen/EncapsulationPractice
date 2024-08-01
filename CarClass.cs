a959db5bea21d813f92141425ad4815885be1672
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-01 13:00:00 +0300

    Add files via upload

---
+++ CarClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice
+{
+    internal class CarClass
+    {
+        //Araba sınıfının özellikleri -> Marka, Model, Renk, Kapı Sayısı
+        private string? _brand;
+        private string? _model;
+        private string? _color;
+        private int _numberOfCarDoors;
+
+        public string? Brand
+        { get { return _brand; } set { _brand = value; } }
+        public string? Model
+        { get { return _model; } set { _model = value; } }
+        public string? Color
+        { get { return _color; } set { _color = value; } }
+
+        //Özelliklerimizden (Property) Kapı sayısı için kapsülleme işlemi yapmak istiyoruz.
+        public int NumberOfCarDoors
+        {
+            get { return _numberOfCarDoors; }
+
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _numberOfCarDoors = value;
+                }
+                else
+                {
+                    Console.WriteLine("YANLIŞ KAPI SAYISI GİRDİNİZ!");
+                    _numberOfCarDoors--;
+
+                }
+
+            }
+
+        }
+        public CarClass(string brand, string model, string color, int door)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            NumberOfCarDoors = door;
+
+        }
+
+
+        public void DisplayCar()
+        {
+            Console.WriteLine($"{_brand} marka aracınız, {_numberOfCarDoors} Kapı sayısıyla üretilmiştir");
+
+        }
+
+
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using EncapsulationPractice;
+using System.Security.Cryptography;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        CarClass example = new CarClass("AUDİ","A3","BLACK",4);
+
+        example.DisplayCar();
+
+
+
+        Console.WriteLine("");
+        //Eğer bir Araba nesnesi oluşturulduğunda Kapı
+        //Sayısı için 2 veya 4 dışında bir değer atanmak isterse
+        //konsol ekranında bir uyarı mesajı yayınlayalım ve Kapı
+        //Sayısı değerini -1 olarak atayalım.
+
+
+        CarClass example2 = new CarClass("BMW", "F30", "WHITE", 5);
+
+        example.DisplayCar();
+
+
+
+
+    }
+}
+
